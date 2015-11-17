32b10ae658ba21735dd54dd8dcb3a93a33363503
Mary Andrade <maryt.rodriguez@gmail.com>
2015-11-17 11:47:23 +0100

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,50 @@
+// Responsive menu
+
+
+
+
+// Accordion function
+
+$(document).ready(function(){
+  var animTime = 300,
+      clickPolice = false;
+
+  $(document).on('touchstart click', '.acc-btn', function(){
+    if(!clickPolice){
+       clickPolice = true;
+
+      var currIndex = $(this).index('.acc-btn'),
+          targetHeight = $('.acc-content-inner').eq(currIndex).outerHeight();
+
+      $('.acc-btn h2').removeClass('select');
+      $(this).find('h2').addClass('select');
+
+      $('.acc-content').stop().animate({ height: 0 }, animTime);
+      $('.acc-content').eq(currIndex).stop().animate({ height: targetHeight }, animTime);
+
+      setTimeout(function(){ clickPolice = false; }, animTime);
+    }
+
+  });
+
+});
+
+// Go-up button function
+
+$(document).ready(function(){
+
+	$('.go-up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.go-up').slideDown(300);
+		} else {
+			$('.go-up').slideUp(300);
+		}
+	});
+
+});
